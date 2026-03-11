951e60fb0f619d6baa58712a3f785088d6013e0e
ImFocused <prathamjainpbj2306@gmail.com>
2026-03-11 16:28:15 +0530

    weather icon touches

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apikey ="34485d06b7ad93b169190c0479695ba1";
 const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchbtn = document.querySelector("#search button");
 const searchbox = document.querySelector("#search input");
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkweather(city){
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
@@ -13,8 +14,29 @@ async function checkweather(city){
     document.querySelector("#temp").innerHTML = Math.floor(data.main.temp) + ` °C`;
     document.querySelector("#humidity-no").innerHTML = data.main.humidity + ` %`;
     document.querySelector("#wind-no").innerHTML = data.wind.speed + `Km/Hr`;
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "clouds.png"
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "clear.png"
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "drizzle.png"
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "mist.png"
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "rain.png"
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "snow.png"
+
+
     
 
+}
 }
 
 searchbtn.addEventListener("click",()=>{
