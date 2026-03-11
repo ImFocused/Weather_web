9fe2a6d52b0ded8feb23e2d8c9b4304badf9403b
ImFocused <prathamjainpbj2306@gmail.com>
2026-03-11 11:41:04 +0000

    js done

--- script.js
+++ script.js
@@ -6,33 +6,47 @@ const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkweather(city){
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
-    var data = await response.json();
+
+    if(!response.ok){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector("#desc").style.visibility = "hidden";
+        document.querySelector(".other").style.visibility = "hidden";
+    }
+    else{
+        var data = await response.json();
+
+        document.querySelector(".error").style.display = "none";
+        document.querySelector("#desc").style.visibility = "visible";
+        document.querySelector(".other").style.visibility = "visible";
 
 
-    console.log(data);
-    document.querySelector("#city-name").innerHTML = data.name ;
-    document.querySelector("#temp").innerHTML = Math.floor(data.main.temp) + ` °C`;
-    document.querySelector("#humidity-no").innerHTML = data.main.humidity + ` %`;
-    document.querySelector("#wind-no").innerHTML = data.wind.speed + `Km/Hr`;
+        console.log(data);
+        document.querySelector("#city-name").innerHTML = data.name ;
+        document.querySelector("#temp").innerHTML = Math.floor(data.main.temp) + ` °C`;
+        document.querySelector("#humidity-no").innerHTML = data.main.humidity + ` %`;
+        document.querySelector("#wind-no").innerHTML = data.wind.speed + `Km/Hr`;
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "clouds.png"
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "clear.png"
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "drizzle.png"
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "mist.png"
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "rain.png"
-    }
-    else if(data.weather[0].main == "Snow"){
-        weatherIcon.src = "snow.png"
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "clouds.png"
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "clear.png"
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "drizzle.png"
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "mist.png"
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "rain.png"
+        }
+        else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "snow.png"
 
+    }
+
+   
 
     
 
